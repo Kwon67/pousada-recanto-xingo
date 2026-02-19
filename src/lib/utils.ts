b770f960a2ad50e4416b08673ee12f048a01dd97
Kwon67 <kontakriada0001@gmail.com>
2026-02-19 19:48:36 -0300

    shadcnui adicionado para ser o superman do tailwindcss

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getWhatsAppLink(phone: string, message?: string) {
+  const cleanPhone = phone.replace(/\D/g, '');
+  const url = new URL(`https://wa.me/${cleanPhone}`);
+  if (message) {
+    url.searchParams.set('text', message);
+  }
+  return url.toString();
+}
+
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+}
+
+export function calcularNoites(checkIn: Date, checkOut: Date) {
+  const diffTime = Math.abs(checkOut.getTime() - checkIn.getTime());
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+}
+
+export function calcularValorTotal(precoPorNoite: number, checkIn: Date, checkOut: Date) {
+  return precoPorNoite * calcularNoites(checkIn, checkOut);
+}
